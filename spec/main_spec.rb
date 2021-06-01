1fe083df3b5ca383f8e955d09e815bb01a149eed
mahbubul14 <mahbub21alam2017@gmailcom>
2021-06-01 21:14:00 +0600

    Add two more rspec test

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -6,11 +6,18 @@ describe Scraper do
       text = Scraper.new
       expect(text.p_names).not_to be_nil
     end
+    it 'String limit 50 words' do
+      text = Scraper.new
+      expect(text.p_names.size).to be < 50
+    end
   end
   describe '#prices' do
     let(:costs) { Scraper.new }
     it 'returns products prices' do
       expect(costs.p_prices).not_to be_nil
+    end
+    it 'Price will be more than 0' do
+      expect(costs.p_prices).not_to eql 0
     end
   end
 end
