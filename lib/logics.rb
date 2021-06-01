3f3f6ec794e4efbf9d40e841fc46c2e060bdaa03
mahbubul14 <mahbub21alam2017@gmailcom>
2021-06-01 19:41:30 +0600

    Update logics file

--- lib/logics.rb
+++ lib/logics.rb
@@ -2,7 +2,7 @@ require 'httparty'
 require 'nokogiri'
 
 class Scraper
-  attr_accessor :parse_page
+  attr_reader :parse_page
 
   def initialize
     doc = HTTParty.get('http://www.nike.com/w/mens-nike-by-you-lifestyle-shoes-13jrmz6ealhznik1zy7ok')
